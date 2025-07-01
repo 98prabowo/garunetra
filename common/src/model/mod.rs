09f8d9799a48b69991b0e4633014cec52181ad04
98prabowo <dimasprabowo98@icloud.com>
2025-07-01 14:48:03 +0700

    feat(common): create transaction response DTO

--- common/src/model/mod.rs
+++ common/src/model/mod.rs
@@ -1,2 +1,4 @@
+mod tx_response;
 mod wallet_type;
+pub use tx_response::TxResponse;
 pub use wallet_type::WalletType;
---
+++ common/src/model/tx_response.rs
@@ -0,0 +1,15 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TxResponse {
+    pub from: String,
+    pub to: String,
+    pub token_symbol: String,
+    pub time_stamp: String,
+    pub value: String,
+    pub contract_address: String,
+    pub gas_price: Option<String>,
+    pub gas_used: Option<String>,
+    pub hash: String,
+}
