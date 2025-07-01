2b9b02877fd8f2c956d26187134f1e2266e82a3e
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2025-07-01 22:23:51 +0700

    fix(netrascan): remove unused `WalletAddress` model

--- netrascan/src/model/mod.rs
+++ netrascan/src/model/mod.rs
@@ -1,9 +1,7 @@
 mod etherscan_response;
-mod wallet_address;
 mod wallet_feature;
 mod wallet_report;
 
 pub use etherscan_response::EtherscanResponse;
-pub use wallet_address::WalletAddress;
 pub use wallet_feature::WalletFeature;
 pub use wallet_report::WalletReport;
