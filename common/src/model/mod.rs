8377db1cf4756159db4bdbf84f6e571992cc09cf
98prabowo <dimasprabowo98@icloud.com>
2025-07-19 18:23:28 +0700

    feat(common): add ethereum tx model

--- common/src/model/mod.rs
+++ common/src/model/mod.rs
@@ -1,6 +1,7 @@
 mod block;
 mod flow_summary;
 mod tx_category;
+mod tx_ethereum;
 mod tx_record;
 mod tx_response;
 mod tx_sample;
@@ -9,6 +10,7 @@ mod wallet_type;
 pub use block::BlockEthereum;
 pub use flow_summary::{FlowSummary, Wei};
 pub use tx_category::TxCategory;
+pub use tx_ethereum::TxEthereum;
 pub use tx_record::TxRecord;
 pub use tx_response::TxResponse;
 pub use tx_sample::TxSample;
---
+++ common/src/model/tx_ethereum.rs
@@ -0,0 +1,36 @@
+use serde::Deserialize;
+
+use crate::utils::Heuristics;
+
+use super::TxCategory;
+
+// use super::TxCategory;
+
+#[derive(Debug, Deserialize)]
+pub struct TxEthereum {
+    pub hash: String,
+    pub from: String,
+    pub to: Option<String>,
+    pub input: String,
+    pub value: String,
+}
+
+impl TxEthereum {
+    pub fn categorize(&self, heuristics: &Heuristics) -> TxCategory {
+        self.to
+            .as_ref()
+            .map(|addr| addr.to_ascii_lowercase())
+            .map(|to| {
+                if heuristics.is_known_cex(&to) {
+                    TxCategory::Foreign
+                } else if heuristics.is_known_bridge(&to) {
+                    TxCategory::Bridge
+                } else if heuristics.is_known_domestic(&self.from, &to) {
+                    TxCategory::Domestic
+                } else {
+                    TxCategory::Foreign
+                }
+            })
+            .unwrap_or(TxCategory::Unknown)
+    }
+}
