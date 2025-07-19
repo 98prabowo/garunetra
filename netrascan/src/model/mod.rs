f927d6949767485173d0b4f6b3f6f6de060d3968
98prabowo <dimasprabowo98@icloud.com>
2025-07-19 18:28:46 +0700

    fix(netrascan): remove block and tx for common model

--- netrascan/src/model/mod.rs
+++ netrascan/src/model/mod.rs
@@ -1,9 +1,5 @@
-mod block;
-mod tx;
 mod wallet_feature;
 mod wallet_report;
 
-pub use block::Block;
-pub use tx::{Tx, TxClassified};
 pub use wallet_feature::WalletFeature;
 pub use wallet_report::WalletReport;
